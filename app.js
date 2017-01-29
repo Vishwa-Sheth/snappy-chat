c617be55a42567035252b6cabc06a2652dba74dc
Vishwa-Sheth <shethvishwa3@gmail.com>
2017-01-29 17:03:13 +0530

    Modified

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 click("signin-button",logInUser);
+
+var user1 ={
+  id: 1,
+  name: "Vishwa",
+  username: "VishwaSheth",
+  sayHello: function(){
+    console.log("Hello " + this.name);
+  }
+};
+
+var user2 ={
+  id: 2,
+  name: "Hilloni",
+  username: "HilloniShah",
+  sayHello: function(){
+    console.log("Hello " + this.name);
+  }
+};
+
+var users = [
+  user1,
+  user2
+];
+
+for(var i=0;i<users.length;i++){
+  var user =users[i];
+  console.log(user.id);
+}
+var message = new Object();
+message.text ="Hello";
+console.log(message.text);
